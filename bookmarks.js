3914a915a9075c49b1ceaaa53f5d5fe3824f2ba3
Austin Zhang <zenith00dev@gmail.com>
2018-05-28 17:15:49 -0700

    Update

--- bookmarks.js
+++ bookmarks.js
@@ -5,6 +5,7 @@ chrome.contextMenus.create({
 });
 
 function onClickHandler(info, tab) {
+    console.log(info);
     if (info.menuItemId == "dumpbookmarks"){
         dumpBookmarks();
     }
@@ -15,7 +16,15 @@ chrome.contextMenus.onClicked.addListener(onClickHandler);
 
 chrome.runtime.onInstalled.addListener(function() {
     chrome.contextMenus.create({"title": "dumpbookmarks", "id": "dumpbookmarks"});
+    console.log("'" + context + "' item:" + id);
 
+    console.log("About to try creating an invalid item - an error about " +
+        "duplicate item child1 should show up");
+    chrome.contextMenus.create({"title": "Oops", "id": "child1"}, function() {
+        if (chrome.extension.lastError) {
+            console.log("Got expected error: " + chrome.extension.lastError.message);
+        }
+    });
 });
 
 function dumpBookmarks() {
